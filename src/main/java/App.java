0c588d705d548d39df25b2e9fafb45a0d9a579a4
Chalermchai Jindaprakai <chalermchai.jin@ascendcorp.com>
2018-10-17 14:35:42 +0700

    init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,40 @@
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRSaver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws FileNotFoundException, JRException {
+        System.out.println("====== Start ======");
+
+        String staticPath = "/data/Tax/Report/";
+
+        String projectPath = "/Users/chalernchai.jin/acm-tmn-tax/TaxEJB/src/main/resources/report/";
+
+        List<String> stringList = new ArrayList<String>();
+        stringList.add("TAX_T101");
+        stringList.add("TAX_T101_List");
+
+        InputStream inputStream = null;
+        for (String strReport : stringList) {
+            File file = new File(staticPath + strReport + ".jrxml");
+            inputStream = new FileInputStream(file);
+            JasperReport jasperReport = JasperCompileManager
+                    .compileReport(inputStream);
+
+            JRSaver.saveObject(jasperReport, projectPath + strReport + ".jasper");
+
+            System.out.println("====== Compile " + strReport + " Success. ======");
+        }
+
+        System.out.println("====== Finish ======");
+    }
+}
